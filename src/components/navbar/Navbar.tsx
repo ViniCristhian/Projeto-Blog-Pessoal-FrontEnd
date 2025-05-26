fd1815244b630db04f5788149168b1e233171bf4
ViniCristhian <viniciuscristhian34@gmail.com>
2025-05-26 15:05:21 -0300

    feat: implementação do CRUD básico de Tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
-
 function Navbar() {
 
     const navegar = useNavigate();
@@ -23,11 +22,11 @@ function Navbar() {
                     <Link to="/home" className="text-2xl font-bold"> Blog Pessoal</Link>
 
                     <div className="flex gap-4">
-                        <Link to="/" className="">Postagens</Link>
-                        <Link to="/" className="">Temas</Link>
-                        <Link to="/">Cadastrar tema</Link>
-                        <Link to="/login" className="">Perfil</Link>
-                        <Link to="" onClick={logout} className="">Sair</Link>
+                        <Link to="/" className="hover:underline">Postagens</Link>
+                        <Link to="/temas" className="hover:underline">Temas</Link>
+                        <Link to="/cadastrartema" className="hover:underline">Cadastrar tema</Link>
+                        <Link to="/login" className="hover:underline">Perfil</Link>
+                        <Link to="" onClick={logout} className="hover:underline">Sair</Link>
                     </div>
                 </div>
             </div>
